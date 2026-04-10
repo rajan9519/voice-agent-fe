adcb3f426e11d0b5993bd52664bab6177a6d68b7
Rajan Kumar Singh <rajankumarsinghvnit@gmail.com>
2026-04-10 06:00:28 +0530

    voice agent ecosystem for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {                                                                                                                           
+    apps: [{     
+      name: 'voice-agent-fe',
+      script: 'npm',
+      args: 'start',
+      cwd: '/Users/rajan/dev/voice-agent-fe',
+      env: {                                                                                                                                   
+        NODE_ENV: 'production',
+        PORT: 3000                                                                                                                             
+      }          
+    }]
+  }
